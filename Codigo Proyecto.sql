a0d1b6f5124022cb2fa85fe619dc96c5219d5b60
Javier Peñaloza Tallia <javier.penalozat@gmail.com>
2026-02-10 21:45:29 +0100

    Javier answers without question 10 and external analysis

--- Codigo Proyecto.sql
+++ Codigo Proyecto.sql
@@ -1,23 +1,23 @@
 USE ih_gambling;
-/*PREGUNTA1*/
+/*Question1*/
 /*Mostrar columnas especificas del archivo customer*/
 SELECT Title, FirstName, LastName, DateOfBirth
 FROM customer;
 
-/*PREGUNTA2*/
+/*Question2*/
 /*Contabilizar el total de customers de cada grupo establecido*/
 SELECT CustomerGroup, COUNT(*) AS NumberofCustomers
 FROM customer
 GROUP BY CustomerGroup;
 
-/*PREGUNTA3*/
+/*Question3*/
 /*Unir tablas para añadir Currency (account) a tabla customer*/
 SELECT customer.*, account.CurrencyCode
 FROM customer
 LEFT JOIN account
 ON customer.CustId = account.CustId;
 
-/*PREGUNTA4*/
+/*Question4*/
 /*Resumir total de apuestas segun la fecha y el producto*/
 SELECT AccountNo, BetDate, ClassId, CategoryId, Bet_Amt, Product
 FROM betting;
@@ -33,7 +33,7 @@ AND betting.CategoryId = product.CATEGORYID
 GROUP BY product.product, BetDate
 ORDER BY BetDate, product.product;
 
-/*PREGUNTA5*/
+/*Question5*/
 /*Realizar cambios donde muestre solo transacciones luego del 1 de noviembre
 y del producto Sportsbook*/
 SELECT BetDate, product.product, SUM(Bet_Amt) AS TotalBet
@@ -45,7 +45,7 @@ WHERE STR_TO_DATE(BetDate, "%d/%m/%Y") >= "2012/11/01" AND product.product = "Sp
 GROUP BY product.product, BetDate
 ORDER BY BetDate;
 
-/*PREGUNTA6*/
+/*Question6*/
 /*Cambios en la presentacion de productos, ahora relacionados
 a currencycode y customergroup y luego del 1 de diciembre*/
 /*Un customer ID puede tener varias cuentas, por lo que hay que JOIN segun las cuentas ya que
@@ -63,7 +63,7 @@ WHERE STR_TO_DATE(BetDate, "%d/%m/%Y") > "2012/12/01"
 GROUP BY product.product, account.CurrencyCode, customer.CustomerGroup
 ORDER BY product.product, account.CurrencyCode, customer.CustomerGroup;
 
-/*PREGUNTA7*/
+/*Question7*/
 /*Mostrar a todos los jugadores sin importar si han hecho alguna apuesta o no
 y tienen que ser solo los del mes de noviembre.
 Tiene que mostrar Title, Nombre, apellido y el resumen de sus apuestas en ese periodo*/
@@ -76,7 +76,7 @@ ON account.AccountNo = betting.AccountNo
 AND STR_TO_DATE(BetDate, "%d/%m/%Y")  >= "2012/11/01" AND STR_TO_DATE(BetDate, "%d/%m/%Y") < "2012/12/01"
 GROUP BY customer.CustId, customer.Title, customer.FirstName, customer.LastName;
 
-/*PREGUNTA8*/
+/*Question8*/
 /*1-Number of product per player*/
 /*2- Players who play Sportsbook and Vegas*/
 SELECT customer.CustId, Title, FirstName, LastName, COUNT(DISTINCT product.product) AS TotalProducts
@@ -103,7 +103,7 @@ WHERE product.product IN ("Sportsbook", "Vegas")
 GROUP BY customer.CustId, Title, FirstName, LastName
 HAVING COUNT(DISTINCT product.product) = 2;
 
-/*PREGUNTA9*/
+/*Question9*/
 /*Players who only play Sportsbook, bet_amt > 0 as key*/
 
 SELECT customer.CustId, Title, FirstName, LastName, SUM(Bet_Amt) AS TotalBet
@@ -120,14 +120,14 @@ GROUP BY customer.CustId, Title, FirstName, LastName
 HAVING COUNT(DISTINCT product.product) = 1
 AND MAX(product.product) = "Sportsbook";
 
-/*PREGUNTA10*/
+/*Question10*/
 /*Player favorite product*/
 
-/*ANALISIS EXTERNO*/
-/*1- Que producto es el mas rentable?(EJERCICIOS 4)*/
-/*2- Que meses son los peores para campaña de reactivacion(EJERCICIO 7)*/
-/*3- En relacion a lo anterior, cuantos clientes estan inactivos los peores meses*/
-/*4- Confirmar si los grupos estan bien definidos (gold, bronxe,silver)*/
+/*EXTERNAL ANALYISIS*/
+/*1- What product its more productive?(EXCERCISE4)*/
+/*2- Worst months less profitable and why?(EJERCISE7)*/
+/*3- Number of clients inactive in the worst month*/
+/*4- Confirm if groups are really true to there bets(gold, bronxe,silver)*/
 
 /*1*/
 SELECT product.product, SUM(Bet_Amt) AS TotalBet
